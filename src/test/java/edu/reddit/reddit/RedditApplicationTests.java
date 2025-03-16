8358a5fcfe844c88edb485487333671338207180
Sigma <87021477+CLSigmaaa@users.noreply.github.com>
2025-03-16 17:21:19 +0100

    Init

---
+++ src/test/java/edu/reddit/reddit/RedditApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.reddit.reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
